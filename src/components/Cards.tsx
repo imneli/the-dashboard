2b6ecdb97cf052b2fa55e236bff8b294eacde2fa
imneli <contatodoneli@gmail.com>
2024-08-26 09:45:40 -0300

    Font Weight: Transactions

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -6,11 +6,11 @@ function Cards() {
     return(
         <>
             <div className="bg-gray-50 shadow-sm min-h-[20vh] min-w-[10vw] rounded-xl flex items-center justify-evenly p-5">
-                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#8aff96'/> 187 </div> <BadgeTrue/></div>
-                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#8aff96'/> 7</div> <BadgeTrue/></div>
-                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#ff918a'/> 92</div> <BadgeFalse/></div>
-                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#8aff96'/> 27</div> <BadgeTrue/></div>
-                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#ff918a'/> 17</div> <BadgeFalse/></div>
+                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3 className='font-semibold'>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#8aff96'/> 187 </div> <BadgeTrue/></div>
+                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3 className='font-semibold'>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#8aff96'/> 7</div> <BadgeTrue/></div>
+                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3 className='font-semibold'>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#ff918a'/> 92</div> <BadgeFalse/></div>
+                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3 className='font-semibold'>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#8aff96'/> 27</div> <BadgeTrue/></div>
+                    <div className="bg-white min-h-[15vh] min-w-[14vw] rounded-xl shadow-sm flex p-5 flex-col"><h3 className='font-semibold'>New Transaction</h3> <div className='flex items-center'><DollarSign size={20} className='mt-3 mb-3' color='#ff918a'/> 17</div> <BadgeFalse/></div>
                 </div>
         </>
     )
