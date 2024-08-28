f6c8240cbce5187b2dd5a2c57aeacde8649749dd
imneli <contatodoneli@gmail.com>
2024-08-28 13:48:51 -0300

    Error Page: Layout

---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@tremor/react"
+import { NavLink } from 'react-router-dom';
+
+export function Error() {
+    return( 
+        <>
+            <div className="flex justify-center items-center min-h-screen flex-col">
+                <h1 className="text-[#1e1e1e] font-bold text-9xl">4<span className="text-[#59f6f6]">0</span>4</h1>
+                <h1 className="text-2xl m-5">Page not found</h1>
+                <Button variant="secondary" color="cyan"><NavLink to="/">Return to Home</NavLink></Button>
+            </div>
+        </>
+    )
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home';
 import Goals from '../pages/Goals';
 import Graph from '../pages/Graph'
 import Wallet from '../pages/Wallet'
+import { Error } from '../pages/Error';
 
 function AppRoutes() {
     return (
@@ -12,6 +13,7 @@ function AppRoutes() {
             <Route path="/goals" element={<Goals />} />
             <Route path="/wallet" element={<Wallet />} />
             <Route path="/charts" element={<Graph />} />
+            <Route path="*" element={<Error />} />
         </Routes>
     );
 }
